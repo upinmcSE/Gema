af8ec5e0027940b421428642bf32ad771199d63c
Duong Trung Thanh <thanh.dt@eastgate-software.com>
2026-01-06 15:20:08 +0700

    feat: create download task proto file

---
+++ download-task/src/main/java/io/upinmcSE/DownloadTaskApplication.java
@@ -0,0 +1,13 @@
+package io.upinmcSE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DownloadTaskApplication.class, args);
+    }
+
+}
---
+++ download-task/src/test/java/io/upinmcSE/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.upinmcSE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
