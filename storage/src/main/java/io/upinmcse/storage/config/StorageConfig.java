7d36d9fa822752036535bc41e02e0b0e71364e4e
Duong Trung Thanh <thanh.dt@eastgate-software.com>
2026-01-30 18:48:47 +0700

    feat: init storage service

--- storage/src/main/java/io/upinmcse/storage/config/StorageConfig.java
+++ storage/src/main/java/io/upinmcse/storage/config/StorageConfig.java
@@ -1,4 +1,27 @@
 package io.upinmcse.storage.config;
 
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class StorageConfig {
+
+    @Value("${minio.endpoint}")
+    private String minioEndpoint;
+
+    @Value("${minio.access-key}")
+    private String minioAccessKey;
+
+    @Value("${minio.secret-key}")
+    private String minioSecretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioEndpoint)
+                .credentials(minioAccessKey, minioSecretKey)
+                .build();
+    }
 }
